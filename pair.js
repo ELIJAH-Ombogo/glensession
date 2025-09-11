6d629347e19fc4c16cf102352314aed47d6bd3e8
Elijah ombogo <elijahombogo2004@gmail.com>
2025-09-11 08:10:07 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "GLEN" + string_session;
+                        let md = "GLEN~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*ᎻᎬᏞᏞϴ ႮՏᎬᎡՏ! 👋🏻* 
 
